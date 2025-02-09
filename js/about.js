d49691f9506306616cfacb855f05c9c0e9c16aae
userabdullahameer <itsabdullahameer@gmail.com>
2025-02-09 10:40:13 +0500

    change

--- js/about.js
+++
@@ -1,7 +0,0 @@
-// JavaScript functionality for the website
-
-// Toggle the navigation menu on smaller screens
-document.getElementById("hamburger").addEventListener("click", function () {
-    const navLinks = document.querySelector(".nav-links");
-    navLinks.classList.toggle("active");
-});
